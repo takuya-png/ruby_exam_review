60615b61bcd9ca5b7e9b984cc4d002064ea728b1
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-10-25 23:22:16 +0900

    if文について

--- test.rb
+++ test.rb
@@ -1,4 +1,13 @@
-age = 22
-if age < 20
-  puts "あなたは未成年です"
+age = 18
+if age < 20 # trueを返却する条件式
+  puts "あなたは未成年です(1)" # 処理が実行される
+end
+if true # trueという値そのもの
+  puts "あなたは未成年です(2)" # 処理が実行される
+end
+if age > 20  # falseを返却する条件式
+  puts "あなたは未成年です(3)" # 処理は実行されない
+end
+if false # falseという値そのもの
+  puts "あなたは未成年です(4)" # 処理は実行されない
 end
