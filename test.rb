9a0bea52a4bfc27b4d6eb2b9bc85343c6248b91e
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-10-25 22:54:00 +0900

    変数を使う理由について

--- test.rb
+++ test.rb
@@ -1,10 +1,5 @@
-# 変数nameに文字列"tom"を代入
-name = "tom"
-# tomを代入した変数nameをputsメソッドで表示
-puts name
-# 変数numberに数値100を代入
-number = 100
-# 100を代入した変数numberをputsメソッドで表示
-puts number
-content = "柿くえば　鐘が鳴るなり　法隆寺"
-puts content
+puts "私の好きな車はフェラーリ"
+puts "高級車といったらフェラーリ"
+car = "フェラーリ"
+puts "私の好きな車は" + car
+puts "高級車といったら" + car
