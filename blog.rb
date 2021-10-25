1fbf2be2205545c01b9b5279f276ebf657af6c35
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-10-26 00:11:11 +0900

    ループ処理を追加

--- blog.rb
+++ blog.rb
@@ -1,21 +1,24 @@
-puts "以下より行う操作を選んでください"
-puts "1:ブログを作成する"
-puts "2:作成されたブログを見る"
-puts "3:ブログアプリを終了する"
-number = gets.to_i
-if number == 1
+while true # 追記する
+  puts "以下より行う操作を選んでください"
   puts "1:ブログを作成する"
-  puts "ブログのタイトルを入力してください"
-  blog_title = gets
-  puts "ブログの本文を入力してください"
-  blog_content = gets
-  puts "入力されたタイトルと本文は以下です"
-  puts "タイトル：#{blog_title}"
-  puts "本文：#{blog_content}"
-elsif number == 2
   puts "2:作成されたブログを見る"
-elsif number == 3
   puts "3:ブログアプリを終了する"
-else
-  puts "1~3の数字を入力してください"
-end
+  number = gets.to_i
+  if number == 1
+    puts "1:ブログを作成する"
+    puts "ブログのタイトルを入力してください"
+    blog_title = gets
+    puts "ブログの本文を入力してください"
+    blog_content = gets
+    puts "入力されたタイトルと本文は以下です"
+    puts "タイトル：#{blog_title}"
+    puts "本文：#{blog_content}"
+  elsif number == 2
+    puts "2:作成されたブログを見る"
+  elsif number == 3
+    puts "3:ブログアプリを終了する"
+    break # 追記する
+  else
+    puts "1~3の数字を入力してください"
+  end
+end 
