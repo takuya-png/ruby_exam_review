6a3fe0909833d5df1addd653282842f672147e6a
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-10-25 23:45:38 +0900

    ブログアプリの実装

--- blog.rb
+++ blog.rb
@@ -1,8 +1,21 @@
-puts '以下より行う操作を選んでください'
-puts '1:ブログを作成する'
-puts '2:作成されたブログを見る'
-puts '3:ブログアプリを終了する'
-#getsメソッドでユーザから値を受け取り、変数numberに代入しています。
-number = gets
-#変数numberをputsメソッドで出力しています。
-puts number
+puts "以下より行う操作を選んでください"
+puts "1:ブログを作成する"
+puts "2:作成されたブログを見る"
+puts "3:ブログアプリを終了する"
+number = gets.to_i
+if number == 1
+  puts "1:ブログを作成する"
+  puts "ブログのタイトルを入力してください"
+  blog_title = gets
+  puts "ブログの本文を入力してください"
+  blog_content = gets
+  puts "入力されたタイトルと本文は以下です"
+  puts "タイトル：#{blog_title}"
+  puts "本文：#{blog_content}"
+elsif number == 2
+  puts "2:作成されたブログを見る"
+elsif number == 3
+  puts "3:ブログアプリを終了する"
+else
+  puts "1~3の数字を入力してください"
+end
