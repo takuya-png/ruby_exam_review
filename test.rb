23fcab95ad8640297daecd28689e67f848397a9d
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-10-25 23:24:35 +0900

    比較演算子について

--- test.rb
+++ test.rb
@@ -1,13 +1,7 @@
-age = 18
-if age < 20 # trueを返却する条件式
-  puts "あなたは未成年です(1)" # 処理が実行される
-end
-if true # trueという値そのもの
-  puts "あなたは未成年です(2)" # 処理が実行される
-end
-if age > 20  # falseを返却する条件式
-  puts "あなたは未成年です(3)" # 処理は実行されない
-end
-if false # falseという値そのもの
-  puts "あなたは未成年です(4)" # 処理は実行されない
-end
+age = 20
+puts age < 30
+puts age <= 15
+puts age > 10
+puts age > 30
+puts age == 20
+puts age != 20
