b3739d0ed9d4f22bb08714c3c3a035fa27618f66
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-10-27 15:34:46 +0900

    配列とハッシュ

--- test.rb
+++ test.rb
@@ -1,2 +1,3 @@
-players_count = {baseball: 9,soccer: 11,basketball: 5}
-puts players_count[:soccer]
+test = [{subject: "math", points: 70}, {subject: "english", points: 50}, {subject: "society", points: 80}]
+test << {subject: "science", points: 100}
+puts test[3][:points]
