b0d94f07f24c186e77d3bacf43d600bc2e75b2f0
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-10-28 10:16:16 +0900

    文字列とシンボル

--- test.rb
+++ test.rb
@@ -1,3 +1,3 @@
-test = [{subject: "math", points: 70}, {subject: "english", points: 50}, {subject: "society", points: 80}]
-test << {subject: "science", points: 100}
-puts test[3][:points]
+{ title: "こんにちは", content: "寒いですね" }.each do |key, value|
+  puts "#{key} #{value}"
+end
