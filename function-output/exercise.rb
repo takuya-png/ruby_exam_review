bc2ce5189873481020a9039338d853347e572a91
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-10-28 18:42:31 +0900

    演習1

---
+++ function-output/exercise.rb
@@ -0,0 +1,5 @@
+house = [{price:"2000万",location: "東京都",size:"80坪"},{price:"1500万",location:"千葉県",size:"50坪"},{price:"800万",location:"埼玉県",size:"100坪"}]
+puts house[1][:location]
+puts house[0][:size]
+puts house[2][:price]
+puts house[2][:size]
