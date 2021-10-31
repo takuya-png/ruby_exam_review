c1bf5100d5da90df789c99c0e44edc6666c6c8d3
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-10-31 16:55:38 +0900

    オブジェクトの振る舞いを定義する

--- school.rb
+++ school.rb
@@ -1,11 +1,25 @@
+require 'Launchy'
 class School
-  def name
-    @greeting = "Hello!"
+  attr_accessor :name,
+                :address,
+                :number_of_students,
+                :founding_years,
+                :introduction_video_url,
+                :introduction_statement
+  def initialize(name, address, number_of_students,founding_years,
+                 introduction_video_url, introduction_statement)
+    @name = name
+    @address = address
+    @number_of_students = number_of_students
+    @founding_years = founding_years
+    @introduction_video_url = introduction_video_url
+    @introduction_statement = introduction_statement
   end
-  def name1
-    puts @greeting
+  def launch_school_url
+    Launchy.open(@introduction_video_url)
   end
 end
-school = School.new
-school.name
-school.name1
+a_school = School.new("A学校", "東京都新宿区..", 300, 100, "https://www.hoge.com", "A学校は自然豊かな...")
+a_school.launch_school_url
+b_school = School.new("B学校", "東京都新宿区..", 500, 30, "https://www.hoge.com", "B学校は文武両立で...")
+b_school.launch_school_url
