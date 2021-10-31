9f8408b7c4500840a25c61e1975759094793b764
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-10-31 16:37:36 +0900

    アクセスメソッドについて

--- test.rb
+++ test.rb
@@ -1,3 +1,12 @@
-{ title: "こんにちは", content: "寒いですね" }.each do |key, value|
-  puts "#{key} #{value}"
+class School
+  # nameというインスタンスメソッドを定義する
+  def name(word)
+    @school_name = word
+  end
+  def name1
+    puts @school_name
+  end
 end
+school = School.new
+school.name("A学校")
+school.name1
